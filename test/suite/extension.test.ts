127c916e8910ade10a61eef4df39c987b92474f6
Lei Zhu <julytreee@gmail.com>
2023-04-25 22:51:59 +0800

    print res

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -150,8 +150,8 @@ suite('Extension Test Suite', () => {
 
   test('Extension should activate', async () => {
     let succeed;
-    await getExtension()?.activate().then((x) => { console.log(`${x}\n`); succeed = true }, () => { console.log("222\n"); succeed = false });
-    console.log(`\n\n\n${succeed}\n\n\n`);
+    const res: any = await getExtension()?.activate();
+    console.log(`\n\n\nres\n\n\n`);
     assert.ok(succeed);
   });
 
